a39cb83042b6fbaa67356cdafb7716cc41551a91
Jose <jose62sax@gmail.com>
2022-03-15 10:16:47 -0500

    Creacion de repositorio y añadiendo codigo primera entrega

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 let nombres = [];
 let nombresFinal = 0;
 let buscador;
-
+// LLENAR EL ARRAY CON LOS NOMBRES
 alert('Vamos a guardar listado de nombres.');
 let cantidad = prompt('Digita el numero de nombres que deseas ingresar: ');
-
 for (let i = 0; i < cantidad; i++) {
     nombres[i] = prompt('Digita el nombre: ');
 }
+//SABER LA CANTIDAD DE VECES QUE SE REPITE EL NOMBRE DIGITADO EN EL ARRAY
     alert('Ahora busquemos un nombre que se repite. ');
     buscador = prompt('Digite el nombre a Buscar: ');
 for (let i = 0; i < nombres.length; i++) {
